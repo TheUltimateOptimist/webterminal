ddbc199e17bc4ae5018c465bab0dd8dfcd0c7333
TheUltimateOptimist <digitalConfidence@web.de>
2023-04-09 16:18:03 +0200

    finished authentication

--- lib/terminal.dart
+++ lib/terminal.dart
@@ -1,21 +1,76 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 // ignore: depend_on_referenced_packages
 import 'package:statewithease/statewithease.dart';
+import 'package:webterminal/helper_widgets/custom_icon_button.dart';
+import 'package:webterminal/terminal_widgets/colored_text.dart';
 import 'terminal_theme.dart';
 import 'terminal_widgets/widgets.dart' show getTerminalWidget, InputRow, Command;
+import 'package:firebase_auth/firebase_auth.dart';
 
 class TerminalState {
-  const TerminalState(this.contents, this.channel);
+  TerminalState(this.contents, this.channel, this.user);
 
   final List<Widget> contents;
-  final WebSocketChannel channel;
+  WebSocketChannel? channel;
+  StreamSubscription? channelSubscription;
+  final User? user;
+
+  factory TerminalState.initial() {
+    final user = FirebaseAuth.instance.currentUser;
+    return TerminalState(
+      [user != null ? const CommandInputRow() : const EmailInputRow()],
+      null,
+      user,
+    );
+  }
 }
 
 TerminalState commandEntered(TerminalState old, String command) {
+  old.contents.removeLast();
   old.contents.add(Command(text: command.trim()));
-  old.channel.sink.add(command.trim());
-  return TerminalState(old.contents, old.channel);
+  old.channel!.sink.add(command.trim());
+  return TerminalState(old.contents, old.channel, old.user);
+}
+
+TerminalState emailEntered(TerminalState old, String email) {
+  old.contents.removeLast();
+  old.contents.add(ColoredText("E-Mail: $email"));
+  old.contents.add(PasswordInputRow(email));
+  return TerminalState(old.contents, old.channel, old.user);
+}
+
+Future<TerminalState> passwordEntered(TerminalState old, String email, String password) async {
+  old.contents.removeLast();
+  old.contents.add(ColoredText("Password: ${'•' * password.length}"));
+  try {
+    final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+    old.contents.add(const CommandInputRow());
+    return TerminalState(old.contents, old.channel, userCredential.user);
+  } on FirebaseAuthException catch (error) {
+    old.contents.add(ColoredText(error.code, color: Colors.red));
+    old.contents.add(const EmailInputRow());
+    return TerminalState(old.contents, old.channel, old.user);
+  }
+}
+
+StateStream<TerminalState> getTerminalWidgetStream() {
+  final channel = WebSocketChannel.connect(Uri.parse("ws://localhost:3000/terminal"));
+  return StateStream<TerminalState>(
+    stream: channel.stream.map(
+      (response) => (state) {
+        state.contents.add(getTerminalWidget(response));
+        state.contents.add(const CommandInputRow());
+        return TerminalState(state.contents, state.channel, state.user);
+      },
+    ),
+    assign: (state, subscription) {
+      state.channelSubscription = subscription;
+      state.channel = channel;
+    },
+  );
 }
 
 class Terminal extends StatelessWidget {
@@ -23,42 +78,105 @@ class Terminal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final channel = WebSocketChannel.connect(Uri.parse("ws://localhost:3000/terminal"));
     return Container(
       color: TerminalTheme.of(context).backgroundColor,
       child: StateProvider<TerminalState>(
-        TerminalState(List.empty(growable: true), channel),
-        mapperStream: channel.stream.map((event) => (state) {
-              state.contents.add(getTerminalWidget(event));
-              return TerminalState(state.contents, channel);
-            }),
-        child: StateBuilder<TerminalState>(
-          builder: (p0, state) {
-            return ListView.builder(
-              itemCount: state.contents.length + 1,
-              itemBuilder: (context, index) {
-                final Widget widget;
-                if (state.contents.isEmpty) {
-                  widget = const InputRow();
-                } else if (index == state.contents.length && state.contents.last is! Command) {
-                  widget = const InputRow();
-                } else if (index == state.contents.length && state.contents.last is Command) {
-                  widget = const SizedBox();
-                } else {
-                  widget = state.contents[index];
-                }
-                return Container(
-                  margin: EdgeInsets.only(
-                    top: TerminalTheme.of(context).lineSpacing,
-                    left: TerminalTheme.of(context).leftSpacing,
-                  ),
-                  child: widget,
-                );
-              },
-            );
+        TerminalState.initial(),
+        stateStreams: FirebaseAuth.instance.currentUser != null ? [getTerminalWidgetStream()] : [],
+        child: StateListener<TerminalState>(
+          listenWhen: (previous, current) => previous.user == null && current.user != null,
+          listener: (context, state) {
+            context.collectStateStream((state) => getTerminalWidgetStream());
           },
+          child: StateBuilder<TerminalState>(
+            builder: (p0, state) {
+              return ListView.builder(
+                itemCount: state.contents.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(
+                      top: TerminalTheme.of(context).lineSpacing,
+                      left: TerminalTheme.of(context).leftSpacing,
+                    ),
+                    child: state.contents[index],
+                  );
+                },
+              );
+            },
+          ),
         ),
       ),
+    );
+  }
+}
+
+class CommandInputRow extends StatelessWidget {
+  const CommandInputRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InputRow(
+      prefix: const Command(),
+      maxLines: null,
+      onEnterPressed: (context, text) {
+        context.collectOne(commandEntered, text);
+      },
+    );
+  }
+}
+
+class EmailInputRow extends StatefulWidget {
+  const EmailInputRow({super.key});
+
+  @override
+  State<EmailInputRow> createState() => _EmailInputRowState();
+}
+
+class _EmailInputRowState extends State<EmailInputRow> {
+  final emailController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return InputRow(
+      prefix: const ColoredText("E-Mail: "),
+      onEnterPressed: (context, text) {
+        context.collectOne(emailEntered, text);
+      },
+    );
+  }
+}
+
+class PasswordInputRow extends StatefulWidget {
+  const PasswordInputRow(this.email, {super.key});
+
+  final String email;
+
+  @override
+  State<PasswordInputRow> createState() => _PasswordInputRowState();
+}
+
+class _PasswordInputRowState extends State<PasswordInputRow> {
+  bool _isVisible = true;
+  @override
+  Widget build(BuildContext context) {
+    return InputRow(
+      isVisible: _isVisible,
+      prefix: Row(
+        children: [
+          const ColoredText("Password: "),
+          CustomIconButton(
+            _isVisible ? Icons.visibility : Icons.visibility_off,
+            onPressed: () {
+              setState(() {
+                _isVisible = !_isVisible;
+              });
+            },
+          ),
+          const ColoredText(" "),
+        ],
+      ),
+      onEnterPressed: (context, text) {
+        context.collectFutureTwo(passwordEntered, widget.email, text);
+      },
     );
   }
 }
