fc96976c72eddbc2e8c7fff78986fce694d2c956
TheUltimateOptimist <digitalConfidence@web.de>
2023-04-09 16:16:54 +0200

    added authentication

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 import 'terminal.dart';
 import 'terminal_theme.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
